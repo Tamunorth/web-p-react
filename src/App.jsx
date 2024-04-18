edf82406237ea80239c2682999f2d8b80b91bf89
Tamunorth <daviesmanuelfavour@gmail.com>
2024-04-18 11:32:33 +0100

    skeleton

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <p>Hellow</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
